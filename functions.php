6e187eaec07cb27c3db97222a9488d0fd6d8d02c
Bob Weinand <bobwei9@hotmail.com>
2014-07-08 01:02:33 +0200

    Nutze PDO

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ date_default_timezone_set('UTC');
 require 'config.php';
 
 function connectDatabase() {
-	$db = new \BenchPDO("mysql:host=".DB_HOST.";dbname=".DB_DB.";charset=utf8;charset=utf8", DB_USER, DB_PASS);
+	$db = new \PDO("mysql:host=".DB_HOST.";dbname=".DB_DB.";charset=utf8;charset=utf8", DB_USER, DB_PASS);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	return $db;
