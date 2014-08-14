b165d396d3a1ca55071c9db30b25e60427898bfc
Niklas Keller <me@kelunik.com>
2014-08-15 00:53:16 +0200

    Fix double utf8 @ dbconnect

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 
 $__connect = function($host, $user, $pass, $db) {
-	$db = new PDO("mysql:host={$host};dbname={$db};charset=utf8;charset=utf8", $user, $pass);
+	$db = new PDO("mysql:host={$host};dbname={$db};charset=utf8", $user, $pass);
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	return $db;
