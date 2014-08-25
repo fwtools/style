085e7d1e69f49d9edd7d318f10f7cbe5c8fb560d
Niklas Keller <me@kelunik.com>
2014-08-25 04:55:14 +0200

    Add tools_npc_views

---
+++ src/App/Component/npcs.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Component;
+
+class npcs implements \App\Component {
+	private $db;
+	private $request;
+
+	public function __construct (\PDO $db, \Arya\Request $request) {
+		$this->db = $db;
+		$this->request = $request;
+	}
+
+	public function get () {
+		return "@import '/npcs.css'";
+	}
+}
