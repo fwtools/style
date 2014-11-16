6a4032e4725b6464fb3d45c3dac7d5af50efee59
Niklas Keller <me@kelunik.com>
2014-11-16 01:25:27 +0100

    Add Comment

--- config.sample.php
+++ config.sample.php
@@ -1,5 +1,7 @@
 <?php
 
+# Copy this file to config.php
+
 const DB_USER = "";
 const DB_PASS = "";
 const DB_HOST = "";
@@ -10,4 +12,3 @@ const DB_DB = "";
 define('CACHE_DIR', __DIR__ . "/gen/");
 
 const STYLES_PATH = "";
-
